5e1fefa42fc64f1e5dfb14df699ad3be95075145
kasasiki3 <121206130+kasasiki3@users.noreply.github.com>
2024-06-30 14:57:32 +0900

    Update serial.js

--- docs/serial.js
+++ docs/serial.js
@@ -13,19 +13,8 @@ var serial = {};
   // requestPort関数は、指定されたフィルタに一致するUSBデバイスの選択ダイアログを表示し、選択されたデバイスをシリアルポートオブジェクトとして返します。
   serial.requestPort = function() {
     const filters = [
-      { 'vendorId': 0x2341, 'productId': 0x8036 }, // Arduino Leonardo
-      { 'vendorId': 0x2341, 'productId': 0x8037 }, // Arduino Micro
-      { 'vendorId': 0x2341, 'productId': 0x804d }, // Arduino/Genuino Zero
-      { 'vendorId': 0x2341, 'productId': 0x804e }, // Arduino/Genuino MKR1000
-      { 'vendorId': 0x2341, 'productId': 0x804f }, // Arduino MKRZERO
-      { 'vendorId': 0x2341, 'productId': 0x8050 }, // Arduino MKR FOX 1200
-      { 'vendorId': 0x2341, 'productId': 0x8052 }, // Arduino MKR GSM 1400
-      { 'vendorId': 0x2341, 'productId': 0x8053 }, // Arduino MKR WAN 1300
-      { 'vendorId': 0x2341, 'productId': 0x8054 }, // Arduino MKR WiFi 1010
-      { 'vendorId': 0x2341, 'productId': 0x8055 }, // Arduino MKR NB 1500
-      { 'vendorId': 0x2341, 'productId': 0x8056 }, // Arduino MKR Vidor 4000
-      { 'vendorId': 0x2341, 'productId': 0x8057 }, // Arduino NANO 33 IoT
-      { 'vendorId': 0x239A }, // Adafruit Boards!
+      { 'vendorId': 0x0f0d, 'productId': 0x0092 }, // Arduino Leonardo
+      { 'vendorId': 0x0f0d, 'productId': 0x0092 }, // Arduino Micro
     ];
     return navigator.usb.requestDevice({ 'filters': filters }).then(
       device => new serial.Port(device)
